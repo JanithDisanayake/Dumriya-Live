6f6080a2f1d24164460704c9e93aac8dc3ef1fb7
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-05-28 01:11:24 +0530

    fix the issues on lambda express server
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const serverless = require('serverless-http')
 const app = express()
 // const port = 3000
 
-app.get('/hello', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
 	res.send('Hello World!');
-})
+});
 
 // app.listen(port, () => {
 //	console.log(`Example app listening on port ${port}`)
