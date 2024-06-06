5bc849266b5e442eb78faf4706895898a7b5ccbc
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-06-07 00:53:29 +0530

    working in progress
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- example/index.js
+++ example/index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const serverless = require("serverless-http");
+const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+	console.log("xxx");
+	res.send('Hello World!');
 });
 
 module.exports.handler = serverless(app);
