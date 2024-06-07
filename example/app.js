033d9a5171b626e39f793311ce919127675d1eaf
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-06-07 23:30:34 +0530

    🚀 created working image for lambda
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- example/app.js
+++ example/app.js
@@ -5,8 +5,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	console.log("xxx");
+	console.log("/ endpoint is reached");
 	res.send('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+	console.log("/ endpoint is reached");
+	res.send('Hello from Lambda!');
 });
 
 module.exports.handler = serverless(app);
