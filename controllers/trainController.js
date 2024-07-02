417f40378be5ba05b0f0770e739c7d28c2cbfd07
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-07-02 21:29:40 +0530

    connected the train controller with mongodb
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -1,21 +1,16 @@
-let trains = [
-  { id: 1, name: "Express 1", type: "Express" },
-  { id: 2, name: "Local Train A", type: "Local" },
-];
+const Train = require("../models/trainModel");
 
 exports.getAll = async (req, res) => {
+  const trains = await Train.find();
   res.status(200).json(trains);
 };
 
 exports.getById = async (req, res) => {};
 
 exports.register = async (req, res) => {
-  users.push({
-    id: trains.length + 1,
-    name: req.body.name,
-    type: req.body.type,
-  });
-  res.status(201).json(newRoute);
+  const train = new Train({ name: req.body.name, type: req.body.type });
+  await train.save();
+  res.status(201).json(train);
 };
 
 exports.update = async (req, res) => {};
