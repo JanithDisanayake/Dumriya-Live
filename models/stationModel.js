39f06baf780ac78f214e080b5e66859da2a0b8b1
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-07-02 21:17:57 +0530

    created schema for stations
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

---
+++ models/stationModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const stationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+const Station = mongoose.model("Station", stationSchema);
+module.exports = Station;
