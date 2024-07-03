35849ea5a9539d51f82eebf6bc345ba0bea9e193
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-07-03 22:20:17 +0530

    restructure the route model
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- models/routeModel.js
+++ models/routeModel.js
@@ -1,17 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const GeoPoint = require("./geoPoint");
 
 const routeSchema = new Schema({
   start: {
-    type: String,
-    required: true,
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: GeoPoint,
+      required: true,
+    },
   },
   end: {
-    type: String,
-    required: true,
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: GeoPoint,
+      required: true,
+    },
   },
   distance: {
-    type: Number,
+    type: [GeoPoint],
     required: true,
   },
 });
