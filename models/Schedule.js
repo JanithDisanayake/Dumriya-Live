af7471a7d635e2a6bbcdd23ee814488fd1da92c0
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-07-16 20:41:27 +0530

    created new schema for Schedule
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

---
+++ models/Schedule.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const GeoLocation = require("./GeoLocation");
+const Route = require("./Route");
+const Train = require("./Train");
+const Station = require("./Station");
+
+const scheduleSchema = new Schema({
+  train: {
+    type: Train.schema,
+    required: true,
+  },
+  start_station: {
+    type: Station.schema,
+    required: true,
+  },
+  destination_station: {
+    type: Station.schema,
+    required: true,
+  },
+  route: {
+    type: Route.schema,
+    required: true,
+  },
+  depature_time: {
+    type: Date,
+    requred: true,
+  },
+  arrival_time: {
+    type: Date,
+    requred: true,
+  }
+});
+
+const Schedule = mongoose.model("Schedule", scheduleSchema);
+module.exports = Schedule;
