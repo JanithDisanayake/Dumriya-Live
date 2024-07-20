d4d0a29cf10e327c8e76fd4c484492fdcd1a86d1
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-07-20 23:42:11 +0530

    fix the routing issue
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 app.use("/users", userRoutes);
-app.use("/trains", trainRoutes);
+app.use("/trains/*", trainRoutes);
 app.use("/stations", stationRoutes);
 app.use("/routes", routeRoutes);
 app.use("/schedules", scheduleRoutes);
