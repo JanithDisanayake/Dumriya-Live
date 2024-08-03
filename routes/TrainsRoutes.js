ba1aca75f553ccae000865070264a14cc6c8220d
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-08-04 03:30:40 +0530

    reconfigure the routes based on the accessibility

--- routes/TrainsRoutes.js
+++ routes/TrainsRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const trainController = require("../controllers/TrainsController");
 
-router.get("/", trainController.getAll);
-router.get("/:id", trainController.getById);
-router.post("/", trainController.register);
-router.put("/:id", trainController.update);
+router.get("/live_log", trainController.getLiveLog);
+router.post("/live_log", trainController.storeLiveLog);
 
 router.get("/live", trainController.getLive);
 router.post("/live", trainController.storeLive);
 
-router.get("/live_log", trainController.getLiveLog)
-router.post("/live_log", trainController.storeLiveLog)
+router.get("/", trainController.getAll);
+router.get("/:id", trainController.getById);
+router.post("/", trainController.register);
+router.put("/:id", trainController.update);
 
 module.exports = router;
