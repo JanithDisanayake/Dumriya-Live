bd7edc9cf9125f215efca845618a2dff6b3145c2
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-08-13 20:00:57 +0530

    handle errors in train controlle

--- routes/TrainsRoutes.js
+++ routes/TrainsRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const trainController = require("../controllers/TrainsController");
 
+router.get("/:id", trainController.getById);
+router.put("/:id", trainController.update);
+
 router.get("/live_log", trainController.getLiveLog);
 router.post("/live_log", trainController.storeLiveLog);
 
@@ -10,8 +13,6 @@ router.get("/live/:id", trainController.getLiveById);
 router.post("/live", trainController.storeLive);
 
 router.get("/", trainController.getAll);
-router.get("/:id", trainController.getById);
 router.post("/", trainController.register);
-router.put("/:id", trainController.update);
 
 module.exports = router;
