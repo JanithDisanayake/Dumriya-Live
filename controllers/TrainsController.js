12f91c939e8bee9ff4d32d6a00002271d7a1d26b
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-08-16 23:31:18 +0530

    update routings on train
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- controllers/TrainsController.js
+++ controllers/TrainsController.js
@@ -23,74 +23,86 @@ exports.getById = async (req, res) => {
       return res.status(404).json({ message: "Train not found." });
     }
     res.status(200).json(train);
-    
   } catch (error) {
     console.error("Error fetching train by ID:", error);
-    res.status(500).json({ message: "An error occurred while fetching the train." });
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching the train." });
   }
 };
-
 
 exports.register = async (req, res) => {
   try {
     const train = new Train({
       ...req.body,
     });
-    
+
     await train.save();
     res.status(201).json(train);
-
   } catch (error) {
-    res.status(500).json({ message: "An error occurred while registering the train", error: error.message });
+    res.status(500).json({
+      message: "An error occurred while registering the train",
+      error: error.message,
+    });
   }
 };
-
 
 exports.update = async (req, res) => {
   try {
     const { id } = req.params; // Extract ID from request parameters
     const updateData = req.body; // Extract update data from request body
 
-    const updatedTrain = await Train.findByIdAndUpdate(id, updateData, { new: true, runValidators: true }); // Find and update the Train document
+    const updatedTrain = await Train.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    }); // Find and update the Train document
 
     if (!updatedTrain) {
-      return res.status(404).json({ message: 'Train not found' }); // Handle case where document is not found
+      return res.status(404).json({ message: "Train not found" }); // Handle case where document is not found
     }
 
     res.status(200).json(updatedTrain); // Send the updated document as a response
   } catch (error) {
     console.error("Error updating train:", error); // Log the error for debugging
-    res.status(500).json({ message: "An error occurred while updating the train", error: error.message }); // Handle any other errors
+    res.status(500).json({
+      message: "An error occurred while updating the train",
+      error: error.message,
+    }); // Handle any other errors
   }
 };
 
 exports.getLive = async (req, res) => {
   try {
     const trainLive = await TrainLive.find();
-    
+
     res.status(200).json(trainLive);
   } catch (error) {
     // Log the error and send a response with an appropriate status code and message
-    res.status(500).json({ message: "An error occurred while retrieving live train data", error: error.message });
+    res.status(500).json({
+      message: "An error occurred while retrieving live train data",
+      error: error.message,
+    });
   }
 };
 
 exports.getLiveById = async (req, res) => {
   try {
-    const { id } = req.params; 
+    const { id } = req.params;
     if (!id) {
-      return res.status(400).json({ message: 'Train ID is required' });
+      return res.status(400).json({ message: "Train ID is required" });
     }
-    
+
     const trainLive = await TrainLive.findOne({ "train._id": id });
     if (!trainLive) {
-      return res.status(404).json({ message: 'TrainLive not found' });
+      return res.status(404).json({ message: "TrainLive not found" });
     }
     res.status(200).json(trainLive);
-    
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'An error occurred while fetching the live train data', error: error.message });
+    res.status(500).json({
+      message: "An error occurred while fetching the live train data",
+      error: error.message,
+    });
   }
 };
 
@@ -110,10 +122,9 @@ exports.storeLive = async (req, res) => {
       ...liveData,
       train: train._id,
     });
-    
+
     await trainLive.save();
     res.status(201).json(trainLive);
-
   } catch (error) {
     console.error("Error storing live data:", error);
     res.status(500).json({ message: "Server error" });
@@ -144,7 +155,6 @@ exports.storeLiveLog = async (req, res) => {
     await log.save();
     updateLive(req.body);
     res.status(201).json(log);
-
   } catch (error) {
     console.error("Error storing live log:", error);
     res.status(500).json({ message: "Server error1" });
@@ -154,25 +164,28 @@ exports.storeLiveLog = async (req, res) => {
 const updateLive = async (trainLiveLog) => {
   try {
     const trainDocument = await TrainLive.findOne({
-        "train.engines._id": trainLiveLog.device._id
+      "train.engines._id": trainLiveLog.device._id,
     });
 
     if (trainDocument) {
-        // Update the current location of the trainLive document
-        await TrainLive.updateOne(
-            { "_id": trainDocument._id },
-            { $set: { "current_location": trainLiveLog.current_location } }
-        );
-        console.log(`\n location updated \n ${trainDocument.train.name} : http://maps.google.com/maps?q=${trainDocument.current_location.coordinates}`);
-    }
-    else {
+      // Update the current location of the trainLive document
+      await TrainLive.updateOne(
+        { _id: trainDocument._id },
+        { $set: { current_location: trainLiveLog.current_location } },
+      );
+      console.log(
+        `\n location updated \n ${trainDocument.train.name} : http://maps.google.com/maps?q=${trainDocument.current_location.coordinates}`,
+      );
+    } else {
       // No matching engine found, create a new TrainLive document
-      const schedule = await Schedule.findOne({ "train.engines._id": trainLiveLog.device._id });
-      const train = schedule.train
+      const schedule = await Schedule.findOne({
+        "train.engines._id": trainLiveLog.device._id,
+      });
+      const train = schedule.train;
       if (train) {
         console.log("Train found:", train);
         const newTrainLive = new TrainLive({
-          train: train, 
+          train: train,
           current_location: trainLiveLog.current_location,
           previous_station: null,
           next_station: null,
--- routes/TrainsRoutes.js
+++ routes/TrainsRoutes.js
@@ -2,15 +2,12 @@ const express = require("express");
 const router = express.Router();
 const trainController = require("../controllers/TrainsController");
 
-router.get("/:id", trainController.getById);
-router.put("/:id", trainController.update);
-
-router.get("/live_log", trainController.getLiveLog);
-router.post("/live_log", trainController.storeLiveLog);
-
 router.get("/live", trainController.getLive);
 router.get("/live/:id", trainController.getLiveById);
 router.post("/live", trainController.storeLive);
+
+router.get("/live_log", trainController.getLiveLog);
+router.post("/live_log", trainController.storeLiveLog);
 
 router.get("/", trainController.getAll);
 router.post("/", trainController.register);
