e29ccb49a75b628d2093053aeb73b537d1e87b94
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-08-18 03:11:17 +0530

    secure the routes
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- routes/RoutesRoutes.js
+++ routes/RoutesRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const routeController = require("../controllers/RoutesController");
+const authenticateJWT = require("../middleware/authenticate");
 
 router.get("/", routeController.getAll);
 router.get("/:id", routeController.getById);
-router.post("/", routeController.register);
-router.put("/:id", routeController.update);
+router.post("/", authenticateJWT, routeController.register);
+router.put("/:id", authenticateJWT, routeController.update);
 
 module.exports = router;
--- routes/SchedulesRoutes.js
+++ routes/SchedulesRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const schedulesController = require("../controllers/SchedulesController");
+const authenticateJWT = require("../middleware/authenticate");
 
 router.get("/", schedulesController.getAll);
 router.get("/:id", schedulesController.getById);
-router.post("/", schedulesController.store);
-router.put("/:id", schedulesController.update);
+router.post("/", authenticateJWT, schedulesController.store);
+router.put("/:id",authenticateJWT, schedulesController.update);
 
 module.exports = router;
